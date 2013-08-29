e68bcd83cbc9036fc46c1672be56eeed58700ac0
anthony1810 <qquang269@gmail.com>
2013-08-29 12:33:07 +0700

    Game Sound

--- client/game_menu/scripts/screen.splash.js
+++ client/game_menu/scripts/screen.splash.js
@@ -1,15 +1,25 @@
 jewel.screens["splash-screen"] = (function() {
     var game = jewel.game,
-    dom = jewel.dom,
+    dom = jewel.dom,   
     firstRun = true;
-var main_menu = document.getElementById("main-menu");
-var splash = document.getElementById("splash-screen");
+    var openSound = AudioFX("../audio/azeroth_legend.wav");
+    var click = AudioFX("../audio/click.wav");
+    
+    openSound.play();
+    var main_menu = document.getElementById("main-menu");
+    var splash = document.getElementById("splash-screen");
 
     var continueButton = document.getElementById("btnContinue");
     continueButton.addEventListener("click",function(){ 
+        click.play();
         main_menu.style.display = 'block';
         splash.style.display = 'none';
         game.showScreen("main-menu");    
+    }, false);
+
+    var register = document.getElementById("create-user");
+    register.addEventListener("click",function(){ 
+        click.play(); 
     }, false);
 
     function setup() {
